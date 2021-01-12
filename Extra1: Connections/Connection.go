e6cedc54ed5c545a006237d3ca856292e4ee89d5
Ishan Pandey <ishan.parasar.3359@gmail.com>
2021-01-12 17:32:57 +0530

    Running connection.go finally peeps

--- Extra1: Connections/Connection.go
+++ Extra1: Connections/Connection.go
@@ -7,8 +7,8 @@ import (
 	"net"
 )
 
-func main1() {
-	server, _ := net.ResolveTCPAddr("tcp", "google.com:72")
+func main() {
+	server, _ := net.ResolveTCPAddr("tcp", "google.com:80")
 	client, _ := net.ResolveTCPAddr("tcp", ":50000")
 	conn, err := net.DialTCP("tcp", client, server)
 	if err != nil {
@@ -17,7 +17,7 @@ func main1() {
 	defer conn.Close()
 	fmt.Println(conn.LocalAddr())
 	fmt.Println(conn.RemoteAddr())
-	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	fmt.Fprintf(conn, "GET / HTTP/1.1\r\n\r\n")
 	status, err := bufio.NewReader(conn).ReadString('\n')
 	fmt.Println(status)
 }
